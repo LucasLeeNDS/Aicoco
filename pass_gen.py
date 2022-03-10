75feb4cada793d2dce1a8b616e0d0fa62e1d0bff
Blu3sh0rk <372091606@qq.com>
2022-03-10 11:28:20 +0800

    用二进制表示判断结果

--- pass_gen.py
+++ pass_gen.py
@@ -11,42 +11,50 @@ while True:
     # DIGIT = string.digits  # '0123456789'
     # PUNCTUATION = '!@#$%^&*()_-+{}|:\"\'<>?[];,./\\'
 
-    have_upper = False
-    have_lower = False
-    have_digit = False
-    have_punctuation = False
-    have_enough_char = (len(user_password) >= 8)
+    password_state = 0b00000
+    # have_upper = False
+    # have_lower = False
+    # have_digit = False
+    # have_punctuation = False
+    # have_enough_char = (len(user_password) >= 8)
 
     for char in user_password:
         if char in string.ascii_uppercase:
-            have_upper = True
+            password_state |= 0b10000
+            # have_upper = True
         elif char in string.ascii_lowercase:
-            have_lower = True
+            password_state |= 0b01000
+            # have_lower = True
         elif char in string.digits:
-            have_digit = True
+            password_state |= 0b00100
+            # have_digit = True
         else:
-            have_punctuation = True
+            password_state |= 0b00010
+            # have_punctuation = True
 
-    is_secure = have_punctuation and have_digit and have_lower and have_upper \
-                and have_enough_char
+    if len(user_password) >= 8:
+        password_state |= 0b00001
+
+    # is_secure = have_punctuation and have_digit and have_lower and have_upper and have_enough_char
+    # is_secure = (password_state == 0b11111)
 
     # TODO:输出
-    if is_secure:
+    if password_state == 0b11111:
         print("密码符合安全规则")
         break
     else:
         # print("密码不符合安全规则，请重新输入!")
         prompt = '密码不符合安全规则，'
-        if not have_enough_char:
+        if password_state & 0b00001 == 0:
             # prompt = prompt + '长度不足8字符，'
             prompt += '长度不足8字符，'
-        if not have_upper:
+        if password_state & 0b10000 == 0:
             prompt += '没有包含大写符号，'
-        if not have_lower:
+        if password_state & 0b01000 == 0:
             prompt += '没有包含小写符号，'
-        if not have_digit:
+        if password_state & 0b00100 == 0:
             prompt += '没有包含数字，'
-        if not have_punctuation:
+        if password_state & 0b00010 == 0:
             prompt += '没有包含特殊符号，'
         prompt = prompt[:-1]
         print(prompt)
