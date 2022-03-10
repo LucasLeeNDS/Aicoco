3fadadd325cc56153280dcfc707fc598d45aee0f
Blu3sh0rk <372091606@qq.com>
2022-03-10 11:49:09 +0800

    函数化改造

--- pass_gen.py
+++ pass_gen.py
@@ -1,24 +1,11 @@
 import string
 
-while True:
-    # TODO:用户输入密码
-    user_password = input('请输入新密码:')
-    # print(user_password)
 
-    # TODO:判断密码是否合格:大写、小写、数字、特殊符号、长度
-    # UPPER = string.ascii_uppercase  # 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    # LOWER = string.ascii_lowercase  # 'abcdefghijklmnopqrstuvwxuz'
-    # DIGIT = string.digits  # '0123456789'
-    # PUNCTUATION = '!@#$%^&*()_-+{}|:\"\'<>?[];,./\\'
-
+def evaluate_password(password):
+    result = False
     password_state = 0b00000
-    # have_upper = False
-    # have_lower = False
-    # have_digit = False
-    # have_punctuation = False
-    # have_enough_char = (len(user_password) >= 8)
 
-    for char in user_password:
+    for char in password:
         if char in string.ascii_uppercase:
             password_state |= 0b10000
             # have_upper = True
@@ -32,21 +19,16 @@ while True:
             password_state |= 0b00010
             # have_punctuation = True
 
-    if len(user_password) >= 8:
+    if len(password) >= 8:
         password_state |= 0b00001
-
-    # is_secure = have_punctuation and have_digit and have_lower and have_upper and have_enough_char
-    # is_secure = (password_state == 0b11111)
 
     # TODO:输出
     if password_state == 0b11111:
         print("密码符合安全规则")
-        break
+        result = True
     else:
-        # print("密码不符合安全规则，请重新输入!")
         prompt = '密码不符合安全规则，'
         if password_state & 0b00001 == 0:
-            # prompt = prompt + '长度不足8字符，'
             prompt += '长度不足8字符，'
         if password_state & 0b10000 == 0:
             prompt += '没有包含大写符号，'
@@ -58,3 +40,14 @@ while True:
             prompt += '没有包含特殊符号，'
         prompt = prompt[:-1]
         print(prompt)
+    return result
+
+def main():
+    while True:
+        # TODO:用户输入密码
+        user_password = input('请输入新密码:')
+        # TODO:判断密码是否合格:大写、小写、数字、特殊符号、长度
+        if evaluate_password(user_password):
+            break
+
+main()
