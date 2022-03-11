1911cc834dfc20caeba53dbf32fdf6f975c506f5
Blu3sh0rk <372091606@qq.com>
2022-03-11 13:57:10 +0800

    加入自动生成密码功能

--- pass_gen.py
+++ pass_gen.py
@@ -1,18 +1,19 @@
+import random
 import string
 
 
-def evaluate_password(password):
+def evaluate_password(password, show_info=True):
     result = False
     password_state = 0b00000
 
     for char in password:
-        if char in string.ascii_uppercase:
+        if char.isupper():
             password_state |= 0b10000
             # have_upper = True
-        elif char in string.ascii_lowercase:
+        elif char.islower():
             password_state |= 0b01000
             # have_lower = True
-        elif char in string.digits:
+        elif char.isdigit():
             password_state |= 0b00100
             # have_digit = True
         else:
@@ -24,30 +25,55 @@ def evaluate_password(password):
 
     # TODO:输出
     if password_state == 0b11111:
-        print("密码符合安全规则")
+        if show_info:
+            print("密码符合安全规则")
         result = True
     else:
-        prompt = '密码不符合安全规则，'
-        if password_state & 0b00001 == 0:
-            prompt += '长度不足8字符，'
-        if password_state & 0b10000 == 0:
-            prompt += '没有包含大写符号，'
-        if password_state & 0b01000 == 0:
-            prompt += '没有包含小写符号，'
-        if password_state & 0b00100 == 0:
-            prompt += '没有包含数字，'
-        if password_state & 0b00010 == 0:
-            prompt += '没有包含特殊符号，'
-        prompt = prompt[:-1]
-        print(prompt)
+        if show_info:
+            prompt = '密码不符合安全规则，'
+            if password_state & 0b00001 == 0:
+                prompt += '长度不足8字符，'
+            if password_state & 0b10000 == 0:
+                prompt += '没有包含大写符号，'
+            if password_state & 0b01000 == 0:
+                prompt += '没有包含小写符号，'
+            if password_state & 0b00100 == 0:
+                prompt += '没有包含数字，'
+            if password_state & 0b00010 == 0:
+                prompt += '没有包含特殊符号，'
+            prompt = prompt[:-1]
+            print(prompt)
     return result
 
-def main():
+
+def generate_password():
+    all_char_set = string.ascii_lowercase \
+                 + string.ascii_uppercase \
+                 + string.digits          \
+                 + string.punctuation
+    all_char_set *= 9
+    result = ''.join(random.sample(all_char_set, k=9))
+    return result
+
+
+def main_userinput():
     while True:
         # TODO:用户输入密码
         user_password = input('请输入新密码:')
         # TODO:判断密码是否合格:大写、小写、数字、特殊符号、长度
         if evaluate_password(user_password):
             break
+
+
+def main():
+    while True:
+        # TODO:生成密码
+        user_password = generate_password()
+        # print("密码是：",''.join(generate_password()))
+        # TODO:判断密码是否合格:大写、小写、数字、特殊符号、长度
+        if evaluate_password(user_password, show_info=False):
+            print(f"生成密码为：{user_password}")
+            break
+
 
 main()
